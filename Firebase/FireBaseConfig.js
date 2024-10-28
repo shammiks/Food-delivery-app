747a1b183c152c78650414d0d83982c765f0dd0f
Shammi Kumar <shammiks49@gmail.com>
2024-10-28 11:45:50 +0530

    Update FireBaseConfig.js

--- Firebase/FireBaseConfig.js
+++ Firebase/FireBaseConfig.js
@@ -4,12 +4,12 @@ import "firebase/compat/auth";
  
 
 const FirebaseConfig = {
-  apiKey: "AIzaSyDoPbeJEiBP5GtAf7G9nlhU5lNRX7d9_yM",
-  authDomain: "foodapp2-4f8f1.firebaseapp.com",
-  projectId: "foodapp2-4f8f1",
-  storageBucket: "foodapp2-4f8f1.appspot.com",
-  messagingSenderId: "25350339877",
-  appId: "1:25350339877:web:400f4dbd8d31ccc604aecc"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: ",
+  appId: ""
   };
 
 if(!firebase.apps.length){
